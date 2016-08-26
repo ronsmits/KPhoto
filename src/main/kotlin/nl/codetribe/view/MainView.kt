f1c94ae18404895795b79ba6383e902c49f8fb0a
Ron Smits <ron.smits@nasdaq.com>
2016-08-26 12:17:12 +0200

    small changes

--- src/main/kotlin/nl/codetribe/view/MainView.kt
+++ src/main/kotlin/nl/codetribe/view/MainView.kt
@@ -6,7 +6,6 @@ import javafx.scene.control.TreeItem
 import javafx.scene.input.DragEvent
 import javafx.scene.input.TransferMode
 import javafx.scene.layout.BorderPane
-import nl.codetribe.controller.TestImageController
 import nl.codetribe.model.Photo
 import nl.codetribe.model.PhotoCategory
 import nl.codetribe.rootCategory
@@ -14,7 +13,6 @@ import tornadofx.*
 
 class MainView : View() {
     override val root = BorderPane()
-    val controller: TestImageController by inject()
     val imageView: ImageTableView by inject()
     val categoryview: CategoryView by inject()
 
@@ -23,8 +21,6 @@ class MainView : View() {
             left = categoryview.root
             center = vbox {
                 prefWidth = 800.0
-
-//                    prefHeight=800.0
                 scrollpane {
                     prefWidth = 600.0
                     hbarPolicy = ScrollPane.ScrollBarPolicy.NEVER
@@ -48,25 +44,31 @@ class CategoryView : View() {
             text = "${it.name} ${it.photolist.size} ${it.dropAllowed}"
             onUserSelect { imageView.update(it) }
             if (it.dropAllowed) {
-                onDragEntered = EventHandler<DragEvent> { event -> println(event) }
+                onDragEntered = EventHandler<DragEvent> { event ->
+//                    this.style = "-fx-background-color: #c3ff10"
+                    event.consume()
+                }
+                onDragExited = EventHandler<DragEvent> { event ->
+//                    this.style = "-fx-background-color: white"
+                    event.consume()
+                }
                 onDragOver = EventHandler<DragEvent> {
                     event ->
-                    val dragboard = event.dragboard
-                    val content = dragboard.getContent(photoformat) as Photo
-                    if (!it.photolist.contains(content)) {
-                        println("$content not found in list")
-                        event.acceptTransferModes(TransferMode.LINK)
-                    } else println("found $content in list")
+                    with(event.dragboard.getContent(photoformat) as Photo) {
+                        if (!it.photolist.contains(this))
+                            event.acceptTransferModes(TransferMode.LINK)
+                    }
                     event.consume()
                 }
                 onDragDropped = EventHandler<DragEvent> { event ->
-                    val dragboard = event.dragboard
-                    val content = dragboard.getContent(photoformat)
-                    it.photolist.add(content as Photo)
-                    this.treeView.refresh()
-                    event.isDropCompleted = true
-                    event.consume()
-
+                    with(event.dragboard.getContent(photoformat) as Photo) {
+                        it.photolist.add(this)
+                        this@treeview.refresh()
+                    }
+                    with(event) {
+                        isDropCompleted = true
+                        consume()
+                    }
                 }
             }
         }
