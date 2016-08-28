2802e210c1f7628d67bbc259351713fc70fde46b
Ron Smits <ron.smits@gmail.com>
2016-08-28 07:11:41 +0200

    datagrid built in, crash happens when switching to a new list

--- src/main/kotlin/nl/codetribe/view/ImageTableView.kt
+++ src/main/kotlin/nl/codetribe/view/ImageTableView.kt
@@ -1,11 +1,10 @@
 package nl.codetribe.view
 
-import javafx.event.EventHandler
 import javafx.scene.image.Image
-import javafx.scene.input.ClipboardContent
+import javafx.scene.image.ImageView
 import javafx.scene.input.DataFormat
 import javafx.scene.input.MouseEvent
-import javafx.scene.input.TransferMode
+import nl.codetribe.model.Photo
 import nl.codetribe.model.PhotoCategory
 import org.controlsfx.control.PopOver
 import tornadofx.*
@@ -16,38 +15,59 @@ import tornadofx.*
 val photoformat = DataFormat("photo")
 
 class ImageTableView : View() {
-    override val root = flowpane {
+    override val root = datagrid<Photo> {
+        cellWidth=200.0
+        cellHeight=200.0
+//        cachedGraphic { ImageView(Image(it.toURL().toExternalForm(), 200.0, 200.0, true, true, true)) }
+        cellFormat {
+            imageview { image=Image(it.toURL().toExternalForm(), 200.0, 200.0, true, true, true) }
+            addEventHandler(MouseEvent.MOUSE_CLICKED, { e ->
+                PopOver().apply {
+                    contentNode = vbox {
+                        label(it.name)
+                        label(it.filepath)
+                    }
+                    show(this@cellFormat)
+                }
+            })
+        }
+    }
+
+    fun update(category: PhotoCategory){
+        root.items=category.photolist.observable()
+    }
+    val rootk = flowpane {
         hgap = 10.0
         vgap = 10.0
         prefWidth = 600.0
 
     }
 
-    fun update(category: PhotoCategory) {
-        with(root) {
-            root.children.clear()
-            category.photolist.sortBy { it.name }
-            category.photolist.forEach {
-                imageview {
-                    onDragDetected =  EventHandler<MouseEvent>(){ e->
-                        this.startDragAndDrop(TransferMode.LINK).apply {
-                            setContent { put(photoformat,it) }
-                        }
-                        e.consume()
-                    }
-
-                    image = Image(it.toURL().toExternalForm(), 200.0, 200.0, true, true, true)
-                    addEventHandler(MouseEvent.MOUSE_CLICKED, { e ->
-                        PopOver().apply {
-                            contentNode = vbox {
-                                label(it.name)
-                                label(it.filepath)
-                            }
-                            show(this@imageview)
-                        }
-                    })
-                }
-            }
-        }
-    }
+//    fun update(category: PhotoCategory) {
+//        with(root) {
+//            root.children.clear()
+//            category.photolist.sortBy { it.name }
+//            category.photolist.forEach {
+//                imageview {
+//                    onDragDetected =  EventHandler<MouseEvent>(){ e->
+//                        this.startDragAndDrop(TransferMode.LINK).apply {
+//                            setContent { put(photoformat,it) }
+//                        }
+//                        e.consume()
+//                    }
+//
+//                    image = Image(it.toURL().toExternalForm(), 200.0, 200.0, true, true, true)
+//                    addEventHandler(MouseEvent.MOUSE_CLICKED, { e ->
+//                        PopOver().apply {
+//                            contentNode = vbox {
+//                                label(it.name)
+//                                label(it.filepath)
+//                            }
+//                            show(this@imageview)
+//                        }
+//                    })
+//                }
+//            }
+//        }
+//    }
 }
