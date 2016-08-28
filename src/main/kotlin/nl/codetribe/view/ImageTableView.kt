4af1d804ff3469961237b671850dede67fe5fd78
Ron Smits <ron.smits@gmail.com>
2016-08-28 12:10:53 +0200

    new version from edvin

--- src/main/kotlin/nl/codetribe/view/ImageTableView.kt
+++ src/main/kotlin/nl/codetribe/view/ImageTableView.kt
@@ -1,7 +1,6 @@
 package nl.codetribe.view
 
 import javafx.scene.image.Image
-import javafx.scene.image.ImageView
 import javafx.scene.input.DataFormat
 import javafx.scene.input.MouseEvent
 import nl.codetribe.model.Photo
@@ -16,11 +15,13 @@ val photoformat = DataFormat("photo")
 
 class ImageTableView : View() {
     override val root = datagrid<Photo> {
+        //cachedGraphic {
+//            imageview(it.toURL().toExternalForm(), true)
+        //}
         cellWidth=200.0
         cellHeight=200.0
-//        cachedGraphic { ImageView(Image(it.toURL().toExternalForm(), 200.0, 200.0, true, true, true)) }
         cellFormat {
-            imageview { image=Image(it.toURL().toExternalForm(), 200.0, 200.0, true, true, true) }
+            imageview { image = Image(it.toURL().toExternalForm(), 200.0, 200.0, true, true, true) }
             addEventHandler(MouseEvent.MOUSE_CLICKED, { e ->
                 PopOver().apply {
                     contentNode = vbox {
@@ -33,41 +34,7 @@ class ImageTableView : View() {
         }
     }
 
-    fun update(category: PhotoCategory){
-        root.items=category.photolist.observable()
+    fun update(category: PhotoCategory) {
+        root.items = category.photolist.observable()
     }
-    val rootk = flowpane {
-        hgap = 10.0
-        vgap = 10.0
-        prefWidth = 600.0
-
-    }
-
-//    fun update(category: PhotoCategory) {
-//        with(root) {
-//            root.children.clear()
-//            category.photolist.sortBy { it.name }
-//            category.photolist.forEach {
-//                imageview {
-//                    onDragDetected =  EventHandler<MouseEvent>(){ e->
-//                        this.startDragAndDrop(TransferMode.LINK).apply {
-//                            setContent { put(photoformat,it) }
-//                        }
-//                        e.consume()
-//                    }
-//
-//                    image = Image(it.toURL().toExternalForm(), 200.0, 200.0, true, true, true)
-//                    addEventHandler(MouseEvent.MOUSE_CLICKED, { e ->
-//                        PopOver().apply {
-//                            contentNode = vbox {
-//                                label(it.name)
-//                                label(it.filepath)
-//                            }
-//                            show(this@imageview)
-//                        }
-//                    })
-//                }
-//            }
-//        }
-//    }
 }
