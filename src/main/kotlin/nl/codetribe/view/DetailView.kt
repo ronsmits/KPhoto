e321074ca15ae54a36b513c4865a0fa9903d13c6
Ron Smits <ron.smits@gmail.com>
2016-09-08 23:02:27 +0200

    ready for merging to master

--- src/main/kotlin/nl/codetribe/view/DetailView.kt
+++ src/main/kotlin/nl/codetribe/view/DetailView.kt
@@ -1,6 +1,9 @@
 package nl.codetribe.view
 
 import javafx.geometry.Orientation
+import javafx.scene.Node
+import javafx.scene.control.Accordion
+import javafx.scene.control.TitledPane
 import javafx.scene.layout.VBox
 import nl.codetribe.controller.PhotoController
 import tornadofx.*
@@ -15,7 +18,7 @@ class DetailView : View() {
     val expandedWith = 250.0
     val accordion = accordion {
         prefWidth = expandedWith
-        fold("basic", basicView.root)
+        fold("basic", basicView.root, expanded = true)
         fold("exif", VBox()) {
             label("test")
             label("test")
@@ -39,7 +42,14 @@ class DetailView : View() {
         }
     }
 }
-
+fun <T : Node> Accordion.fold(title: String? = null, node: T, expanded:Boolean=false, op: (T.() -> Unit)? = null): TitledPane {
+    val fold = TitledPane(title, node)
+    fold.isExpanded=expanded
+    if(expanded==true) this.expandedPane=fold
+    panes += fold
+    op?.invoke(node)
+    return fold
+}
 class BasicViewPane : View() {
     val controller: PhotoController by inject()
 
--- src/main/kotlin/nl/codetribe/view/ImageTableView.kt
+++ src/main/kotlin/nl/codetribe/view/ImageTableView.kt
@@ -1,10 +1,17 @@
 package nl.codetribe.view
 
 import javafx.event.EventHandler
+import javafx.geometry.HPos
+import javafx.geometry.Pos
+import javafx.scene.Scene
 import javafx.scene.image.Image
+import javafx.scene.image.ImageView
 import javafx.scene.input.DataFormat
 import javafx.scene.input.MouseEvent
 import javafx.scene.input.TransferMode
+import javafx.stage.Modality
+import javafx.stage.Stage
+import javafx.stage.StageStyle
 import nl.codetribe.controller.PhotoController
 import nl.codetribe.model.Photo
 import nl.codetribe.model.PhotoCategory
@@ -18,12 +25,8 @@ val photoformat = DataFormat("photo")
 class ImageTableView : View() {
     val controller: PhotoController by inject()
     override val root = datagrid<Photo> {
-        onUserSelect(1) {
-            controller.selectedPhoto.rebind { photo = selectedItem?: Photo("", "") }
-        }
-        onUserSelect(2) {
-            println("double click")
-        }
+        onUserSelect(1) {controller.selectedPhoto.rebind { photo = selectedItem?: Photo("", "") } }
+        onUserSelect(2) { showImagePopup(it)}
         cellCache {
             imageview {
                 image = Image(it.toURL().toExternalForm(), 200.0, 200.0, true, true, true)
@@ -38,6 +41,24 @@ class ImageTableView : View() {
         cellWidth = 200.0
         cellHeight = 200.0
 
+    }
+
+    private fun showImagePopup(photo: Photo) {
+        val image = Image (photo.toURL().toExternalForm())
+        println("dimensions ${image.width} ${image.height}")
+        val im = ImageView(image)
+        val vbox = gridpane { add(im)
+        alignment=Pos.CENTER
+        }
+        with (Stage()) {
+            scene = Scene(vbox, 640.0, 480.0)
+            initModality(Modality.APPLICATION_MODAL)
+            initStyle(StageStyle.DECORATED)
+            im.isPreserveRatio=true
+            im.fitWidthProperty().bind(scene.widthProperty())
+            im.fitHeightProperty().bind(scene.heightProperty())
+            show()
+        }
     }
 
     fun update(category: PhotoCategory) {
