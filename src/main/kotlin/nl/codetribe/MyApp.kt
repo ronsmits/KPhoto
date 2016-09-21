7da3f7b9b22397472d9b101c3d381483dd4faefc
Ron Smits <ron.smits@nasdaq.com>
2016-09-21 08:48:56 +0200

    updated to newest tornadofx (1.5.6)

--- src/main/kotlin/nl/codetribe/MyApp.kt
+++ src/main/kotlin/nl/codetribe/MyApp.kt
@@ -2,7 +2,6 @@ package nl.codetribe
 
 import nl.codetribe.model.Directory
 import nl.codetribe.model.PhotoCategory
-import nl.codetribe.scanner.startScan
 import nl.codetribe.view.MainView
 import tornadofx.App
 import tornadofx.importStylesheet
@@ -11,12 +10,9 @@ val rootCategory = PhotoCategory("root")
 val directoryCategory= Directory(name = "directories", absolutePath = "")
 class MyApp : App(MainView::class) {
     init {
-        //importStylesheet(Styles::class)
+        importStylesheet(Styles::class)
 
         val tag = PhotoCategory("tags", dropAllowed = false)
-        tag.children.add(PhotoCategory("test tag", true))
-        rootCategory.children.add(tag)
-        rootCategory.children.add(directoryCategory)
-        //startScan("./src/main/resources", directoryCategory)
+        rootCategory.children.addAll(directoryCategory, tag)
     }
 }
